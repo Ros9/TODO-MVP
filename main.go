b202b03681c5cca3f4f5a48c4cfd618d9afa142e
Abdrasul Mikhalko <abdrasulmikhalko@gmail.com>
2020-08-01 22:54:40 +0600

    aded delete function

--- main.go
+++ main.go
@@ -10,11 +10,13 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
 type Activity struct{
 	ID primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
 	Name string `json:"name" bson:"name"`
+	Date string `json:"date" bson:"date"`
 }
 
 var(
@@ -35,39 +37,74 @@ func initMongo() (*mongo.Client, error){
 	return client, nil
 }
 
-func drawMainPage(w http.ResponseWriter, r *http.Request){
-	if r.Method == "POST"{
-		err := r.ParseMultipartForm(200000)
-		if err != nil {
-			log.Println(err)
-		}
-		result := r.FormValue("activity")
+func addActivity(w http.ResponseWriter, r *http.Request){
+	err := r.ParseMultipartForm(200000)
+	if err != nil {
+		log.Println(err)
+	}
+	result := r.FormValue("activity")
+	action := r.FormValue("add")
+	if len(action) > 0 {
 		var activity Activity
 		activity.Name = result
+		activity.Date = time.Now().Format("2006.01.02 15:04:05")
 		collection := client.Database("test").Collection("activities")
 		_, err = collection.InsertOne(context.TODO(), activity)
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+	}
+	http.Redirect(w, r, "/TODO", 301)
+}
+
+func deleteActivity(w http.ResponseWriter, r *http.Request){
+	err := r.ParseMultipartForm(200000)
+	if err != nil {
+		log.Println(err)
+	}
+	result := r.FormValue("activity")
+	action := r.FormValue("delete")
+	if len(action) > 0 {
+		collection := client.Database("test").Collection("activities")
+		_, err := collection.DeleteOne(context.TODO(), bson.M{"name": result})
 		if err != nil{
 			fmt.Println(err.Error())
 		}
-		http.Redirect(w, r, "/TODO", 301)
-	} else{
-		collection := client.Database("test").Collection("activities")
-		cursor, err := collection.Find(context.TODO(), bson.M{})
+	}
+	http.Redirect(w, r, "/TODO", 301)
+}
+
+func searchActivity(){
+
+}
+
+func stableMainPage(w http.ResponseWriter, r *http.Request){
+	collection := client.Database("test").Collection("activities")
+	cursor, err := collection.Find(context.TODO(), bson.M{})
+	var data []Activity
+	defer cursor.Close(context.TODO())
+	for cursor.Next(context.TODO()) {
 		var activity Activity
-		var activities []string
-		defer cursor.Close(context.TODO())
-		for cursor.Next(context.TODO()) {
-			if err = cursor.Decode(&activity); err != nil {
-				log.Fatal(err)
-			}
-			activities = append(activities, activity.Name)
+		if err = cursor.Decode(&activity); err != nil {
+			log.Fatal(err)
 		}
-		temp, _ := template.ParseFiles("assets/index.html")
-		temp.Execute(w, activities)
+		data = append(data, activity)
+	}
+	temp, _ := template.ParseFiles("assets/index.html")
+	temp.Execute(w, data)
+}
+
+func drawMainPage(w http.ResponseWriter, r *http.Request){
+	if r.Method == "POST"{
+		addActivity(w, r)
+		deleteActivity(w, r)
+	} else{
+		stableMainPage(w, r)
 	}
 }
 
 func main(){
+	fmt.Println(time.Now().Format("2006.01.02 15:04:05"))
 	http.HandleFunc("/TODO", drawMainPage)
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
 	fmt.Println("Server is listening...")
