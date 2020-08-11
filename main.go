4ae7314fe86044089947cdc5a45da41f14a7e08d
Abdrasul Mikhalko <abdrasulmikhalko@gmail.com>
2020-08-12 05:05:25 +0600

    added pagination and faker endpoint

--- main.go
+++ main.go
@@ -3,7 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"github.com/bxcodec/faker"
+	"github.com/bxcodec/faker/v3"
+	"github.com/gorilla/mux"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -11,6 +12,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 	"time"
 )
 
@@ -26,11 +28,27 @@ type Activity struct{
 type DataOnPage struct{
 	Quantity int64
 	Activities []Activity
+	PrevPage int
+	NextPage int
 }
 
 var(
 	client, _ = initMongo()
 )
+
+func getMin(x, y int) int{
+	if x > y{
+		return y
+	}
+	return x
+}
+
+func getMax(x, y int) int{
+	if x > y{
+		return x
+	}
+	return y
+}
 
 func initMongo() (*mongo.Client, error){
 	clientOptions := options.Client().ApplyURI("mongodb://localhost:27017")
@@ -100,12 +118,12 @@ func editActivity(w http.ResponseWriter, r *http.Request){
 		if err != nil{
 			fmt.Println(err.Error())
 		}
-		http.Redirect(w, r, "/TODO",301)
+		http.Redirect(w, r, "/TODO/page/1",301)
 	} else{
 		var data Activity
 		err := collection.FindOne(context.TODO(), bson.D{{"_id", id}}).Decode(&data)
 		if err != nil{
-			http.Redirect(w, r, "/TODO",301)
+			http.Redirect(w, r, "/TODO/page/1",301)
 			return
 		}
 		temp, _ := template.ParseFiles("assets/editActivity.html")
@@ -161,38 +179,77 @@ func renderData(w http.ResponseWriter, data DataOnPage){
 	temp.Execute(w, data)
 }
 
-func stableMainPage(w http.ResponseWriter, r *http.Request){
+func renderDataOnPage(w http.ResponseWriter, data DataOnPage, param string){
+	temp, _ := template.ParseFiles("assets/index.html")
+	temp.Execute(w, data)
+}
+
+func stableMainPage(w http.ResponseWriter, r *http.Request, param string){
 	collection := client.Database("test").Collection("activities")
 	cursor, err := collection.Find(context.TODO(), bson.M{})
 	defer cursor.Close(context.TODO())
 	if err != nil{
 		fmt.Println(err.Error())
 	}
-	data := DataOnPage{
-		Quantity: calculateQuantityActivities(),
-		Activities: readActivitiesFromCursor(cursor),
+	activities := readActivitiesFromCursor(cursor)
+	upperBound, err := strconv.Atoi(param)
+	if err != nil{
+		fmt.Println(err.Error())
 	}
-	renderData(w, data)
+	upperBound = upperBound * 10
+	lowerBound := upperBound - 9
+	data := DataOnPage{
+		Quantity:   calculateQuantityActivities(),
+	}
+	for i:=0; i<len(activities); i++{
+		if i + 1 >= lowerBound && upperBound >= i + 1{
+			if activities[i].Flag == true {
+				data.Activities = append(data.Activities, activities[i])
+			}
+		}
+	}
+	for i:=0; i<len(activities); i++{
+		if i + 1 >= lowerBound && upperBound >= i + 1{
+			if activities[i].Flag == false {
+				data.Activities = append(data.Activities, activities[i])
+			}
+		}
+	}
+	prevPage, err := strconv.Atoi(param)
+	if err != nil{
+		fmt.Println(err.Error())
+	}
+	prevPage = getMax(prevPage - 1, 1)
+	nextPage, err := strconv.Atoi(param)
+	if err != nil{
+		fmt.Println(err.Error())
+	}
+	nextPage = nextPage + 1
+	data.PrevPage = prevPage
+	data.NextPage = nextPage
+	renderDataOnPage(w, data, param)
 }
 
 func drawMainPage(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
 	if r.Method == "POST"{
 		action := r.FormValue("action")
 		param := r.FormValue("activity_param")
+		redirectPage := "/TODO/page/" + vars["id"]
 		if action == "search"{
 			searchActivity(w, param)
 		} else if action == "add"{
 			addActivity(param)
-			http.Redirect(w, r, "/TODO",301)
+			http.Redirect(w, r, redirectPage,301)
 		} else if action == "delete"{
 			deleteActivity(param)
-			http.Redirect(w, r, "/TODO",301)
+			http.Redirect(w, r, redirectPage,301)
 		} else if action == "move_activity"{
 			changePriorityOfActivity(w, param)
-			http.Redirect(w, r, "/TODO",301)
+			http.Redirect(w, r, redirectPage,301)
 		}
 	} else{
-		stableMainPage(w, r)
+		stableMainPage(w, r, vars["id"])
 	}
 }
 
@@ -207,32 +264,36 @@ func uploadActivitiesToDataBase(activities []Activity){
 	}
 }
 
-func launcherOfFaker(){
-	fmt.Println("Press to [1] for upload fake data to TODO, or [2] for nothing...")
-	var flag int
-	fmt.Scanf("%d", &flag)
-	if flag == 1 {
-		activities := make([]Activity, 20)
-		for i:=0; i<20; i++{
-			var activity Activity
-			err := faker.FakeData(&activity)
-			if err != nil{
-				fmt.Println(err.Error())
-				return
-			}
-			activity.DateOfCreation = getNowTime()
-			activity.DateOfUpdate = getNowTime()
-			activities[i] = activity
-		}
-		uploadActivitiesToDataBase(activities)
+func createFakeData(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	quantity, err := strconv.Atoi(vars["quantity"])
+	if err != nil{
+		fmt.Println(err.Error())
+		return
 	}
+	activities := make([]Activity, quantity)
+	for i:=0; i<quantity; i++{
+		var activity Activity
+		err := faker.FakeData(&activity)
+		if err != nil{
+			fmt.Println(err.Error())
+			return
+		}
+		activity.DateOfCreation = getNowTime()
+		activity.DateOfUpdate = getNowTime()
+		activities[i] = activity
+	}
+	uploadActivitiesToDataBase(activities)
+	http.Redirect(w, r, "/TODO/page/1",301)
 }
 
 func main(){
-	launcherOfFaker()
-	http.HandleFunc("/TODO", drawMainPage)
-	http.HandleFunc("/editActivity", editActivity)
+	router := mux.NewRouter()
+	router.HandleFunc("/TODO/page/{id}", drawMainPage)
+	router.HandleFunc("/editActivity", editActivity)
+	router.HandleFunc("/TODO/createFakeData/{quantity}", createFakeData)
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
+	http.Handle("/", router)
 	fmt.Println("Server is listening...")
 	http.ListenAndServe(":9000", nil)
 }
